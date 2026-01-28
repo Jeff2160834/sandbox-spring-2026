299e8ec2f5f26a8f5b75ddfbec1bc8ed21c44b55
Jeff2160834 <JEF2160834@maricopa.edu>
2026-01-27 18:48:34 -0700

    Update greeting message to include date

--- src/main/java/org/example/sandbox/Hello/Hello.java
+++ src/main/java/org/example/sandbox/Hello/Hello.java
@@ -2,6 +2,6 @@ package org.example.sandbox.Hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! January 2026");
     }
 }
